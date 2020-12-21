12eb4f3ef65ca7d56401534a288cd1d00efe615f
Brian Wong <bwjing@gmail.com>
2020-12-20 18:47:55 -0800

    mock users

--- src/main/java/com/brnwng/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/brnwng/awsimageupload/config/AmazonConfig.java
@@ -5,14 +5,21 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("${AWS_ACCESS_KEY}")
+    private String AWS_ACCESS_KEY;
+
+    @Value("${AWS_SECRET_KEY}")
+    private String AWS_SECRET_KEY;
+
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-          AmazonKeys.ACCESS_KEY.getKey(), AmazonKeys.SECRET_KEY.getKey()
+                AWS_ACCESS_KEY, AWS_SECRET_KEY
         );
         return AmazonS3ClientBuilder
                 .standard()
